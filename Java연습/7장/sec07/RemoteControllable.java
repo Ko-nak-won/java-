401b3af7e45c78996e1d095ec37361b2cc457637
Nakwon <163483323+Ko-nak-won@users.noreply.github.com>
2024-05-27 11:47:27 +0900

    Add files via upload

---
+++ Java연습/7장/sec07/RemoteControllable.java
@@ -0,0 +1,7 @@
+package sec07;
+
+public interface RemoteControllable extends Controllable {
+    void remoteOn();
+
+    void remoteOff();
+}
---
+++ Java연습/7장/sec07/Shape.java
@@ -0,0 +1,12 @@
+package sec07;
+
+abstract class Shape {
+    double pi = 3.14;
+
+    abstract void draw();
+
+    public double findArea(){
+
+        return 0.0;
+    }
+}
